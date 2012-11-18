988b0ae0145bcb4a21ea578eaa32e6dabde526bc
ksuskenka <k.se.ny@mail.ru>
2012-11-18 11:26:44 +0200

    Add some text bla-bla-bla

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,5 +1,5 @@
 class Department < ActiveRecord::Base
-has_many:ustroystvas
+has_many:devices
 has_many:sotrudnikis
 belongs_to:sotrudnikis
   attr_accessible :manager, :name, :short_name
--- app/models/sotrudniki.rb
+++ app/models/sotrudniki.rb
@@ -1,6 +1,6 @@
 class Sotrudniki < ActiveRecord::Base
-has_many:ustroystvas
+has_many:devices
 has_many:departments
 belongs_to:departments
   attr_accessible :access, :adress, :district, :email, :job, :name, :short_name, :tab_number, :telephone, :username
-end
+ends
